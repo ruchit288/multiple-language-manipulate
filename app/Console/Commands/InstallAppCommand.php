26a37dc97a248e0283dc4f9ee5044b898e93284f
ruchitVC <ruchit.patel@viitorcloud.com>
2018-09-13 15:26:05 +0530

    Modified comment

--- app/Console/Commands/InstallAppCommand.php
+++ app/Console/Commands/InstallAppCommand.php
@@ -35,7 +35,7 @@ class InstallAppCommand extends Command
 
     /**
      * InstallAppCommand constructor.
-     * @param Filesystem $filesystem
+     * @param Filesystem $files
      */
     public function __construct(Filesystem $files)
     {
