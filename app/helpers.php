bf4db93a98ee6fc32d6e72fba970e118872182a7
ruchitVC <ruchit.patel@viitorcloud.com>
2018-09-13 15:25:35 +0530

    Integrate install command

---
+++ app/helpers.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!function_exists('checkDatabaseConnection')) {
+
+    /**
+     * @return bool
+     */
+    function checkDatabaseConnection()
+    {
+        try {
+            DB::connection()->reconnect();
+
+            return true;
+        } catch (Exception $ex) {
+            return false;
+        }
+    }
+}
