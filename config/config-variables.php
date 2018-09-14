0bb8a121e3878a4de52816d8caf5ad22cef6528f
ruchitVC <ruchit.patel@viitorcloud.com>
2018-09-14 13:46:33 +0530

    Resolving table name error in configuration file

--- config/config-variables.php
+++ config/config-variables.php
@@ -5,10 +5,4 @@ return [
     'default_db_port'             => 3306,
     'default_db_username'         => 'root',
     'default_lang'                => 'en',
-
-    //field post fix which denote language specific data field (key=> table name/module identification,value => field name)
-    //Key is same as the table name which is define in config-variable file
-    'field_post_fix' => [
-        'industry_sector'     => 'industry_sector_name',
-    ],
 ];
--- config/table-variables.php
+++ config/table-variables.php
@@ -17,4 +17,12 @@ return [
     'industry_sector_table' => 'industry_sectors',
 
     'industry_sector' => IndustrySector::class,
+
+    /*
+     * Field post fix which denote language specific data field (key=> table name/module identification,value => field name)
+     * Key is same as the table name which is define in config-variable file
+    */
+    'field_post_fix' => [
+        'industry_sectors'     => 'industry_sector_name',
+    ],
 ];
